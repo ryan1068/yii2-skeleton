55314be096520e6ec4c7ec06d514a9474322f9b7
Ryan <1336134948@qq.com>
2020-03-31 19:07:22 +0800

    .gitignore

--- api/web/index.php
+++ api/web/index.php
@@ -14,4 +14,4 @@ $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../config/main-local.php'
 );
 
-(new yii\web\Application($config))->run22();
+(new yii\web\Application($config))->run();
